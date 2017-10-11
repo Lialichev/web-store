9a3fa4b8a814bfb33d68b9bfc8b05a9827aa9f5d
Lyalichev <lyalichevv98@gmail.com>
2017-10-11 21:27:53 +0300

    add mobile-sidebar

--- gulpfile.js
+++ gulpfile.js
@@ -63,6 +63,7 @@ gulp.task('watch', ['browser-sync', 'css', 'scripts', 'sprite'], function() {
     gulp.watch('src/sprite/*.png', ['sprite']); // Наблюдение за папкой с картинками для спрайтов  папке sprite
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
+    gulp.watch('src/js/*.js', ['scripts']);
 });
 
 gulp.task('img', function() {
